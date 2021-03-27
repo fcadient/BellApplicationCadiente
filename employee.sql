a8fd734a6264c2d751966480d7a9783323d091ad
Francis Cadiente <francis.m.p.cadiente@gmail.com>
2021-03-27 19:05:58 -0400

    test
    test

---
+++ employee.sql
@@ -0,0 +1,63 @@
+CREATE TABLE Employee (
+    EmployeeID int NOT NULL,
+    EmployeeName varchar(255),
+    Departmentvarchar(255),
+    PRIMARY KEY (EmployeeID)
+
+);
+
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Roma Marcell', 'Division of Telecommunications Extranet Development');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Hugo Wess', 'Extranet Multimedia Connectivity and Security Division');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Kelvin Lahr', 'Branch of Extranet Implementation');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Janelle Newberg', 'Division of Telecommunications Extranet Development');
+
+
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Mellie Lombard', 'Branch of Intranet Computer Maintenance and E-Commerce PC Programming');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Reita Abshire', 'Wireless Extranet Backup Team');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Dalila Vickrey', 'Database Programming Branch');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Idella Dallman', 'Branch of Extranet Implementation');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Farah Eldridge', 'Hardware Backup Department');
+
+
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Lana Montes', 'Hardware Backup Department');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Leola Thornburg', 'Extranet Multimedia Connectivity and Security Division');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Marcelo Paris', 'Database Programming Branch');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Ione Tomlin', 'Multimedia Troubleshooting and Maintenance Team');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Hilario Masterson', 'Multimedia Troubleshooting and Maintenance Team');
+
+
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Janice Skipper', 'Wireless Extranet Backup Team');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Keren Gillespi', 'Office of Statistical Data Connectivity');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Linh Leitzel', 'Division of Telecommunications Extranet Development');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Rosalia Ayoub', 'Division of Application Security');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Shawna Hood', 'Branch of Intranet Computer Maintenance and E-Commerce PC Programming');
+
+
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Wilfredo Stumpf', 'Network Maintenance and Multimedia Implementation Committee');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Alexandra Brendle', 'Office of Statistical Data Connectivity');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Luciano Riddell', 'Mainframe PC Development and Practical Source Code Acquisition Team');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Boyce Perales', 'Network Maintenance and Multimedia Implementation Committee');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Alissa Perlman', 'Division of Telecommunications Extranet Development');
+
+
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Latoyia Kremer', 'Network Maintenance and Multimedia Implementation Committee');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Tawna Blackmore', 'Wireless Extranet Backup Team');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Claudine Valderrama', 'Hardware Backup Department');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Katheryn Lepak', 'Network Maintenance and Multimedia Implementation Committee');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Sage Bow', 'Multimedia Troubleshooting and Maintenance Team');
+
+
+
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Altha Rudisill', 'Hardware Backup Department');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Olympia Vien', 'Division of Application Security');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Olene Pyron', 'PC Maintenance Department');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Delorse Searle', 'Extranet Multimedia Connectivity and Security Division');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Greta Quigg', 'Bureau of Business-Oriented PC Backup and Wireless Telecommunications Control');
+
+
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Kenneth Bowie', 'Branch of Intranet Computer Maintenance and E-Commerce PC Programming');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Colton Kranz', 'Hardware Backup Department');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Kasie Barclay', 'Multimedia Troubleshooting and Maintenance Team');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Thresa Levins', 'Extranet Multimedia Connectivity and Security Division');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Diego Hasbrouck', 'Software Technology and Networking Department');
+INSERT INTO Employee (EmployeeName , Departmentvarchar) VALUES ('Tomoko Gale', 'Database Programming Branch');
